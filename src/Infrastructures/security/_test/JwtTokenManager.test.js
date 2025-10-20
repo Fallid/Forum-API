6a525066feb6831f0e613eaf2fdf2ed4b6848329
Ahmad Naufal <naufalopal002@gmail.com>
2025-10-20 13:10:25 +0700

    test(security): add unit tests for AuthenticationTokenManager and JwtTokenManager
    - Test abstract method enforcement in AuthenticationTokenManager - Test JWT operations, expiry validation, and token extraction in JwtTokenManager

--- src/Infrastructures/security/_test/JwtTokenManager.test.js
+++ src/Infrastructures/security/_test/JwtTokenManager.test.js
@@ -1,6 +1,7 @@
 const Jwt = require('@hapi/jwt');
 const InvariantError = require('../../../Commons/exceptions/InvariantError');
 const JwtTokenManager = require('../JwtTokenManager');
+const AuthenticationError = require('../../../Commons/exceptions/AuthenticationError');
 
 describe('JwtTokenManager', () => {
   describe('createAccessToken function', () => {
@@ -18,8 +19,51 @@ describe('JwtTokenManager', () => {
       const accessToken = await jwtTokenManager.createAccessToken(payload);
 
       // Assert
-      expect(mockJwtToken.generate).toBeCalledWith(payload, process.env.ACCESS_TOKEN_KEY);
+      expect(mockJwtToken.generate).toBeCalledWith(
+        payload,
+        process.env.ACCESS_TOKEN_KEY,
+        { ttlSec: Number(process.env.ACCESS_TOKEN_AGE) },
+      );
       expect(accessToken).toEqual('mock_token');
+    });
+  });
+
+  describe('verifyAccessToken function', () => {
+    it('should throw InvariantError when verification failed', async () => {
+    // Arrange
+      const jwtTokenManager = new JwtTokenManager(Jwt.token);
+      const refreshToken = await jwtTokenManager.createRefreshToken({ username: 'dicoding' });
+
+      // Action & Assert
+      await expect(jwtTokenManager.verifyAccessToken(refreshToken))
+        .rejects
+        .toThrow(InvariantError);
+    });
+
+    it('should throw AuthenticationError when access token is expired', async () => {
+      // Arrange
+      const jwtTokenManager = new JwtTokenManager(Jwt.token);
+      const expiredToken = Jwt.token.generate(
+        { username: 'dicoding' },
+        process.env.ACCESS_TOKEN_KEY,
+        { ttlSec: 1 },
+      );
+      await new Promise((resolve) => setTimeout(resolve, 1100));
+
+      // Action and Assert
+      await expect(jwtTokenManager.verifyAccessToken(expiredToken))
+        .rejects.toThrow(AuthenticationError);
+    });
+
+    it('should not throw InvariantError when access token verified', async () => {
+      // Arrange
+      const jwtTokenManager = new JwtTokenManager(Jwt.token);
+      const accessToken = await jwtTokenManager.createAccessToken({ username: 'dicoding' });
+
+      // Action & Assert
+      await expect(jwtTokenManager.verifyAccessToken(accessToken))
+        .resolves
+        .not.toThrow(InvariantError);
     });
   });
 
@@ -78,6 +122,28 @@ describe('JwtTokenManager', () => {
 
       // Action & Assert
       expect(expectedUsername).toEqual('dicoding');
+    });
+  });
+
+  describe('extractAccessToken function', () => {
+    it('should extract token correctly even if token contains "Bearer "', async () => {
+      // Arrange
+      const jwtTokenManager = new JwtTokenManager(Jwt.token);
+      const accessToken = await jwtTokenManager.createAccessToken({ username: 'dicoding' });
+      const bearerToken = `Bearer ${accessToken}`;
+
+      // Action & Assert
+      await expect(jwtTokenManager.extractAccessToken(bearerToken))
+        .resolves
+        .not.toThrow(AuthenticationError);
+    });
+
+    it('should throw AuthenticationError when token null', async () => {
+      // Arrange
+      const jwtTokenManager = new JwtTokenManager(Jwt.token);
+
+      // Action and Assert
+      await expect(jwtTokenManager.extractAccessToken()).rejects.toThrow(AuthenticationError);
     });
   });
 });
