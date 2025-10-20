b9dce329febfa205140ac3467ac49d708b8e307b
Ahmad Naufal <naufalopal002@gmail.com>
2025-10-20 11:57:14 +0700

    chore(thread): add migration for threads table

---
+++ migrations/1760424279132_create-table-threads.js
@@ -0,0 +1,34 @@
+/* eslint-disable camelcase */
+/**
+ * @param {import('node-pg-migrate').MigrationBuilder} pgm
+ */
+exports.up = (pgm) => {
+  pgm.createTable('threads', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint('threads', 'fk_threads.owner_user.id', 'FOREIGN KEY (owner) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+/**
+ * @param {import('node-pg-migrate').MigrationBuilder} pgm
+ */
+exports.down = (pgm) => {
+  pgm.dropConstraint('threads', 'fk_threads.owner_user.id');
+  pgm.dropTable('threads');
+};
