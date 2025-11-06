cc4c224b981b486e29c2de548e612be358ea38ad
Ahmad Naufal <ahmd.naufal296@gmail.com>
2025-11-06 22:18:16 +0700

    feat(database): create migration for table comments

---
+++ migrations/1762418523162_create-table-comments.js
@@ -0,0 +1,30 @@
+/**
+ * @param {import('node-pg-migrate').MigrationBuilder} pgm
+* */
+
+exports.up = (pgm) => {
+  pgm.createTable('comments', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+
+  pgm.createConstraint('comments', 'fk_comments.owner_user.id', 'FOREIGN KEY (owner) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+/**
+ * @param {import('node-pg-migrate').MigrationBuilder} pgm
+* */
+exports.down = (pgm) => {
+  pgm.dropConstraint('comments', 'fk_comments.owner_user.id');
+  pgm.dropTable('comments');
+};
