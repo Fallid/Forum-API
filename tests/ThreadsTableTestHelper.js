f3fb783721de95b5dea5cf36ff6997f19f2b9590
Ahmad Naufal <ahmd.naufal296@gmail.com>
2025-11-08 12:06:35 +0700

    test(helper): add comment table test helper and improve thread table test helper

--- tests/ThreadsTableTestHelper.js
+++ tests/ThreadsTableTestHelper.js
@@ -1,6 +1,16 @@
 const pool = require('../src/Infrastructures/database/postgres/pool');
 
 const ThreadTableTestHelper = {
+  async addThread({
+    id = 'thread-123', title = 'thread title', body = 'thread body', owner = 'user-123',
+  }) {
+    const query = {
+      text: 'INSERT INTO threads VALUES($1, $2, $3, $4)',
+      values: [id, title, body, owner],
+    };
+    await pool.query(query);
+  },
+
   async cleanTable() {
     await pool.query('DELETE FROM threads WHERE 1=1');
   },
