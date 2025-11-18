f266098f81a4d244ccd393204ffd8454d98d1db2
Ahmad Naufal <ahmd.naufal296@gmail.com>
2025-11-18 21:20:54 +0700

    feat(api-threads): implement get thread by id handler and add new route

--- src/Interfaces/http/api/threads/handler.js
+++ src/Interfaces/http/api/threads/handler.js
@@ -1,10 +1,12 @@
 const AddThreadUseCase = require('../../../../Applications/use_case/AddThreadUseCase');
+const GetThreadByIdUseCase = require('../../../../Applications/use_case/GetThreadByIdUseCase');
 
 class ThreadsHandler {
   constructor(container) {
     this._container = container;
 
     this.postThreadHandler = this.postThreadHandler.bind(this);
+    this.getThreadByIdHandler = this.getThreadByIdHandler.bind(this);
   }
 
   async postThreadHandler(request, h) {
@@ -18,6 +20,21 @@ class ThreadsHandler {
       },
     });
     response.code(201);
+    return response;
+  }
+
+  async getThreadByIdHandler(request, h) {
+    const getThreadById = this._container.getInstance(GetThreadByIdUseCase.name);
+    const useCaseParam = request.params;
+    const detailedThread = await getThreadById.execute(useCaseParam);
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        thread: detailedThread,
+      },
+    });
+    response.code(200);
     return response;
   }
 }
